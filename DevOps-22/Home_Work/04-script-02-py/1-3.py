1e78fd76b57e20a29df9110468a985360f63d144
sergey-svet-melnikov <sergey.svet.melnikov@yandex.ru>
2022-10-25 00:36:53 +0500

    "4.2. Использование Python для решения типовых DevOps задач" Задача 3

--- DevOps-22/Home_Work/04-script-02-py/1-3.py
+++ DevOps-22/Home_Work/04-script-02-py/1-3.py
@@ -6,12 +6,11 @@ import sys
 path = os.getcwd()
 if len(sys.argv) != 1:
     path = sys.argv[1]
-    bash_command = [f'cd '+path, 'git status']
+    bash_command = [f'cd '+path, 'git status 2>&1']
     result_os = os.popen(' && '.join(bash_command)).read()
     for result in result_os.split('\n'):
-        if result.find('fatal') != -1:
-            print('Текущий или указанный каталог: ', path ,' - не являтся репозиторием!')
-            if result.find('modified') != -1:
-                prepare_result = result.replace('\tmodified:   ', '')
-                print('File', prepare_result, 'is modyfied and located at: ', os.getcwd())
-                print(os.getcwd() ,'/' , prepare_result)
+        if result.find('not a git') != -1:
+            print('Текущий или указанный каталог: ', path, '- не являтся репозиторием!')
+        if result.find('modified') != -1:
+            prepare_result = result.replace('\tmodified:   ', '')
+            print('File', prepare_result, 'is modyfied and located at: ', path)
