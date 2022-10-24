1c34440b4929160f4b778b8f5b9f4ecc87b75964
sergey-svet-melnikov <sergey.svet.melnikov@yandex.ru>
2022-10-24 23:51:03 +0500

    "4.2. Использование Python для решения типовых DevOps задач" Задание 3

--- DevOps-22/Home_Work/04-script-02-py/1-2.py
+++ DevOps-22/Home_Work/04-script-02-py/1-2.py
@@ -2,7 +2,7 @@
 
 import os
 
-bash_command = ["cd /Git/devops-netology/", "git status"]
+bash_command = ['cd /Git/devops-netology/', 'git status']
 result_os = os.popen(' && '.join(bash_command)).read()
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
--- DevOps-22/Home_Work/04-script-02-py/1-3.py
+++ DevOps-22/Home_Work/04-script-02-py/1-3.py
@@ -3,13 +3,16 @@
 import os
 import sys
 
-path = sys.argv[1]
-if path != "":
-    bash_command = ["cd "+path, "git status"]
+
+
+path = os.getcwd()
+if len(sys.argv) != 1:
+    path =s ys.argv[1]
+    bash_command = [f'cd '+path, 'git status']
     result_os = os.popen(' && '.join(bash_command)).read()
     for result in result_os.split('\n'):
-        if result.find('modified') != -1:
-            prepare_result = result.replace('\tmodified:   ', '')
-            print(prepare_result)
-else:
-    Print("Не задан путь к каталогу для проверки!")
+        if result.find('fatal') != -1:
+            print('Текущий или указанный каталог: ', path ,'' - не являтся репозиторием!'')
+            if result.find('modified') != -1:
+                print('File', prepare_result, 'is modyfied and located at: ', os.getcwd())
+        print(os.getcwd() ,'/' , prepare_result)
