40688a19a2072a11afafdadd8075009bc2d3e503
sergey-svet-melnikov <sergey.svet.melnikov@yandex.ru>
2022-10-24 23:03:10 +0500

    "4.2. Использование Python для решения типовых DevOps задач" Задача 3

--- DevOps-22/Home_Work/04-script-02-py/1-2.py
+++ DevOps-22/Home_Work/04-script-02-py/1-2.py
@@ -7,4 +7,4 @@ result_os = os.popen(' && '.join(bash_command)).read()
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
-        print(prepare_result)
+        print('file', prepare_result, 'is located at: ', os.getcwd())
