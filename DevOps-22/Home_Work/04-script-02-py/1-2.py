757bf0544adc52a38f55b89928a03281dcda4d34
sergey-svet-melnikov <sergey.svet.melnikov@yandex.ru>
2022-10-24 22:36:06 +0500

    "4.2. Использование Python для решения типовых DevOps задач" Задача 2

--- DevOps-22/Home_Work/04-script-02-py/1-2.py
+++ DevOps-22/Home_Work/04-script-02-py/1-2.py
@@ -2,11 +2,9 @@
 
 import os
 
-bash_command = ["cd c:\Git\devops-netology\", "git status"]
+bash_command = ["cd /Git/devops-netology/", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
-#is_change = False
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
         print(prepare_result)
-#        break
