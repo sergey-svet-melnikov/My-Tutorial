f8cd726b389fc35f4417814ce371a2f2bce32c9a
sergey-svet-melnikov <sergey.svet.melnikov@yandex.ru>
2022-10-24 23:32:37 +0500

    "4.2. Использование Python для решения типовых DevOps задач" Задание 3

--- DevOps-22/Home_Work/04-script-02-py/1-3.py
+++ DevOps-22/Home_Work/04-script-02-py/1-3.py
@@ -1,10 +1,15 @@
 #python3
 
 import os
+import sys
 
-bash_command = ["cd /Git/devops-netology/", "git status"]
-result_os = os.popen(' && '.join(bash_command)).read()
-for result in result_os.split('\n'):
-    if result.find('modified') != -1:
-        prepare_result = result.replace('\tmodified:   ', '')
-        print(prepare_result)
+path = sys.argv[1]
+if path != "":
+    bash_command = ["cd "+path, "git status"]
+    result_os = os.popen(' && '.join(bash_command)).read()
+    for result in result_os.split('\n'):
+        if result.find('modified') != -1:
+            prepare_result = result.replace('\tmodified:   ', '')
+            print(prepare_result)
+else:
+    Print("Не задан путь к каталогу для проверки!")
