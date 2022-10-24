eba02d8985ed15a1010cc36b46b09f897b7033b2
sergey-svet-melnikov <sergey.svet.melnikov@yandex.ru>
2022-10-24 23:51:55 +0500

    "4.2. Использование Python для решения типовых DevOps задач" Задание 3

--- DevOps-22/Home_Work/04-script-02-py/1-3.py
+++ DevOps-22/Home_Work/04-script-02-py/1-3.py
@@ -3,11 +3,9 @@
 import os
 import sys
 
-
-
 path = os.getcwd()
 if len(sys.argv) != 1:
-    path =s ys.argv[1]
+    path = sys.argv[1]
     bash_command = [f'cd '+path, 'git status']
     result_os = os.popen(' && '.join(bash_command)).read()
     for result in result_os.split('\n'):
@@ -15,4 +13,4 @@ if len(sys.argv) != 1:
             print('Текущий или указанный каталог: ', path ,'' - не являтся репозиторием!'')
             if result.find('modified') != -1:
                 print('File', prepare_result, 'is modyfied and located at: ', os.getcwd())
-        print(os.getcwd() ,'/' , prepare_result)
+                print(os.getcwd() ,'/' , prepare_result)
